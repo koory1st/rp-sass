d02a98c2fffc8bc16cb990d881da2a19a4b0fc09
koory1st <32436334@qq.com>
2023-10-19 20:44:11 +0800

    fix(parser): add new type

--- src/scss.rs
+++ src/scss.rs
@@ -8,8 +8,42 @@ pub struct ScssParser;
 
 #[test]
 fn test_class() {
-    assert!(ScssParser::parse(Rule::class, "").is_err());
-    assert!(ScssParser::parse(Rule::class, "a").is_err());
-    assert!(ScssParser::parse(Rule::class, ".").is_err());
-    assert!(ScssParser::parse(Rule::class, ".a").is_ok());
+  assert!(ScssParser::parse(Rule::class, "").is_err());
+  assert!(ScssParser::parse(Rule::class, "a").is_err());
+  assert!(ScssParser::parse(Rule::class, ".").is_err());
+  assert!(ScssParser::parse(Rule::class, "#abc-a_ad").is_err());
+  assert!(ScssParser::parse(Rule::class, ".a").is_ok());
+  assert!(ScssParser::parse(Rule::class, ".abc-a_ad").is_ok());
+}
+
+#[test]
+fn test_id() {
+  assert!(ScssParser::parse(Rule::id, "").is_err());
+  assert!(ScssParser::parse(Rule::id, "a").is_err());
+  assert!(ScssParser::parse(Rule::id, "#").is_err());
+  assert!(ScssParser::parse(Rule::id, ".abc-a_ad").is_err());
+  assert!(ScssParser::parse(Rule::id, "#a").is_ok());
+  assert!(ScssParser::parse(Rule::id, "#abc-a_ad").is_ok());
+}
+
+#[test]
+fn test_identifier() {
+  assert!(ScssParser::parse(Rule::identifier, "").is_err());
+  assert!(ScssParser::parse(Rule::identifier, "#").is_err());
+  assert!(ScssParser::parse(Rule::identifier, ".").is_err());
+  assert!(ScssParser::parse(Rule::identifier, "a").is_ok());
+  assert!(ScssParser::parse(Rule::identifier, ".abc-a_ad").is_err());
+  assert!(ScssParser::parse(Rule::identifier, "a").is_ok());
+  assert!(ScssParser::parse(Rule::identifier, "abc-a_ad").is_ok());
+}
+
+#[test]
+fn test_selector_part() {
+  assert!(ScssParser::parse(Rule::selector_part, "").is_err());
+  assert!(ScssParser::parse(Rule::selector_part, "#").is_err());
+  assert!(ScssParser::parse(Rule::selector_part, ".").is_err());
+
+  assert_eq!(Rule::selector_part, ScssParser::parse(Rule::selector_part, "#abc-a_ad").unwrap().next().unwrap().as_rule());
+  assert_eq!(Rule::selector_part, ScssParser::parse(Rule::selector_part, ".abc-a_ad").unwrap().next().unwrap().as_rule());
+  assert_eq!(Rule::selector_part, ScssParser::parse(Rule::selector_part, "abc-a_ad").unwrap().next().unwrap().as_rule());
 }
