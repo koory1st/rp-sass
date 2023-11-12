f7b7c5075adfbe75c64abcd81371614ba559398c
koory1st <32436334@qq.com>
2023-11-12 20:00:52 +0800

    fix(string): finish

--- src/scss.rs
+++ src/scss.rs
@@ -32,11 +32,57 @@ fn test_literal_string_quoted() {
   assert!(ScssParser::parse(Rule::literal_string_quoted, "\"abcde\"").is_ok());
   assert!(ScssParser::parse(Rule::literal_string_quoted, "\"abcde''\"").is_ok());
   assert!(ScssParser::parse(Rule::literal_string_quoted, "\"#{$a}\"").is_ok());
-  assert!(ScssParser::parse(Rule::literal_string_quoted, "\"#{$a}aaabbbccc\"").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string_quoted, "\"aaaaa#{$a}aaabbbccc\"").is_ok());
   assert!(ScssParser::parse(Rule::literal_string_quoted, "''").is_ok());
   assert!(ScssParser::parse(Rule::literal_string_quoted, "'abcde'").is_ok());
   assert!(ScssParser::parse(Rule::literal_string_quoted, "'abcde\"\"'").is_ok());
-  assert!(ScssParser::parse(Rule::literal_string_quoted, "'abcde\"\"\''").is_ok());
-  assert!(ScssParser::parse(Rule::literal_string_quoted, "'#{$a}'").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string_quoted, "'abcde\\''").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string_quoted, "'abced#{$a}aaaaaa'").is_ok());
   assert!(ScssParser::parse(Rule::literal_string_quoted, "'#{$a}aaabbbccc'").is_ok());
+}
+
+#[test]
+fn test_literal_string_unquoted() {
+  assert!(ScssParser::parse(Rule::literal_string_unquoted, "").is_err());
+  assert!(ScssParser::parse(Rule::literal_string_unquoted, "\"").is_err());
+  assert!(ScssParser::parse(Rule::literal_string_unquoted, "\"\"").is_err());
+  assert!(ScssParser::parse(Rule::literal_string_unquoted, "''").is_err());
+  assert!(ScssParser::parse(Rule::literal_string_unquoted, "abcde").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string_unquoted, "-abcde").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string_unquoted, "--abcde").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string_unquoted, "abcde\\").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string_unquoted, "abced#{$a}aaaaaa").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string_unquoted, "-#{$a}aaab_b-bccc").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string_unquoted, "-#{$prefix}-flex").is_ok());
+}
+
+
+#[test]
+fn test_literal_string() {
+  assert!(ScssParser::parse(Rule::literal_string, "").is_err());
+  assert!(ScssParser::parse(Rule::literal_string, "\"").is_err());
+  assert!(ScssParser::parse(Rule::literal_string, "\"\"").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "\"\\\"\"").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "\"abcde\"").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "\"abcde''\"").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "\"#{$a}\"").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "\"aaaaa#{$a}aaabbbccc\"").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "''").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "'abcde'").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "'abcde\"\"'").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "'abcde\\''").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "'abced#{$a}aaaaaa'").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "'#{$a}aaabbbccc'").is_ok());
+
+  assert!(ScssParser::parse(Rule::literal_string, "abcde").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "-abcde").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "--abcde").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "abcde\\").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "abced#{$a}aaaaaa").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "-abc#{$a}aaab_b-bccc").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "bold").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "-webkit-flex").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "--123").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "ms").is_ok());
+  assert!(ScssParser::parse(Rule::literal_string, "-#{$prefix}-flex").is_ok());
 }
